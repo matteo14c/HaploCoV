9ab960f0d4a729e8cb088705d865eefb1abd7d79
matteo14c <36039934+matteo14c@users.noreply.github.com>
2023-02-15 18:01:03 +0100

    Add files via upload

--- HaploCoV.pl
+++ HaploCoV.pl
@@ -17,6 +17,8 @@ my %arguments=
 ###Process input arguments and check if valid
 check_arguments();
 check_input_arg_valid();
+check_exists_command('mkdir') || die("The unix shell mkdir program is needed to create a directory for storing indermediate files!\n");
+check_exists_command('mv') || die("The mv command is required to rename temporay files!\n");
 ##
 
 my $P=0.05;
@@ -32,11 +34,6 @@ print "$paraFile\n";
 $P=$prev if $prev ne "NA";
 $NW=$wlen if $wlen ne "NA";
 my %parameters=%$parameters;
-#foreach my $P (keys %parameters)
-#{
-#	print "$P $parameters{$P}\n";
-#}
-#die();
 
 my $haploCOV=$arguments{"--path"};
 my $locales=$arguments{"--locales"};
@@ -84,12 +81,16 @@ sub process_data
 			my $ofile=augment($reference,$file,$path,$augmentString);
                		my ($score,$features)=score($ofile,$linString,$reportString);
 			my ($prevalence,$assigned)=prevalence($reference,$ofile,$assignString,$increaseString);
+			my $randTempName=sprintf "%08X", rand(0xffffffff);
 
-
-                	unless (-e "$opath")
-                	{
-                        	system("mkdir $opath")==0||die();
-                	}
+			if (-e "$opath")
+			{
+				print "IMPORTANT!\n The destination directory $opath already exists.\n I will rename the \"old\" directory to $opath.$randTempName\n";
+				system("mv $opath $opath.$randTempName")==0||die("could not rename the directory with temporary files\n");
+			}
+                        
+			system("mkdir $opath")==0||die();
+                	
 			my @files=($ofile,$score,$prevalence,$features,$assigned);
 			push(@files,$path) if $file eq "custom";
 					
@@ -100,7 +101,18 @@ sub process_data
 			}
 
                 	my $otp="$reference\_$file.rep";
+			if (-e "$otp")
+			{
+				print "IMPORTANT!\n The output file $otp does already exist. Renaming the old file to $otp.$randTempName\n";
+				system ("mv $otp $otp.$randTempName")==0||die("could not rename the pre-existing output file $otp\n");
+			}
 			my $ovf=$varfile eq "n" ?  "NA" : "$reference\_$file.var"; 
+			if (-e $ovf)
+			{
+				print "IMPORTANT!\n The output file $ovf does already exist. Renaming the old file to $ovf.$randTempName\n";
+                                system ("mv $ovf $ovf.$randTempName")==0||die("could not rename the pre-existing output file $ovf\n");
+
+			}
                 	generate_final_report($opath,$ofile,$score,$prevalence,$NW,$P,$otp,$ovf,$varfile);
 			print "final results written to $otp\n\n";
         	}
@@ -397,7 +409,7 @@ sub guess_path
 {
 	my $reference=$_[0];
 	my $file=$_[1];
-	my $path="idunno";
+	my $path="mailuu";
 	if ($file eq "custom")
 	{
 		if (-e "metadataTMP")
@@ -405,8 +417,8 @@ sub guess_path
                  	system("rm -rf metadataTMP")==0||die("did not remove old temp file\n");
               	}
 		system("mkdir metadataTMP")==0||die("no metadata directory\n");
-                print "Preprocessing step, type of analysis was set to \"custom\" computing custom mutations file\n";
-		system("perl $haploCOV/computeAF.pl --file $reference --outdir metadataTMP")==0||die("no metadata");
+                print "Preprocessing step, type of analysis was set to \"custom\" computing custom genomic variants file\n";
+		system("perl $haploCOV/computeAF.pl --file $reference --outdir metadataTMP")==0||die("computeAF.pl exited with an error.Please see above!\n");
                 $path="$haploCOV/metadataTMP/areas_list.txt";
 	}
 	$path="$haploCOV/alleleVariantSet/country/$file" if -e "$haploCOV/alleleVariantSet/country/$file";
@@ -414,7 +426,7 @@ sub guess_path
        	$path="$haploCOV/alleleVariantSet/HighFreq/$file" if -e "$haploCOV/alleleVariantSet/HighFreq/$file";
         $path="$haploCOV/$file" if -e "$haploCOV/$file";
         
-	die("could not find $file in your haploCoV installation, please make sure to have a copy of the file in the right place\n") if $path eq "idunno";
+	die("could not find $file in your HaploCoV installation, please make sure to have a copy of the file in the right place\n") if $path eq "mailuu";
 	return ($path);
 }
 
@@ -425,8 +437,8 @@ sub augment
 	my $path=$_[2];
 	my $augmentString=$_[3];
 	print "#Processing $reference $file\n";
-        print " step #1 augment current annotation:\n";
-        system("perl $haploCOV/augmentClusters.pl --outfile $reference\_$file\_results.txt --metafile $reference --posFile $path $augmentString")==0||die("no augment\n");
+        print " step #1 augment the nomenclature\n";
+        system("perl $haploCOV/augmentClusters.pl --outfile $reference\_$file\_results.txt --metafile $reference --posFile $path $augmentString")==0||die("augmentClusters.pl exited with an error. Please see above!\n");
 	return("$reference\_$file\_results.txt");
 }
 
@@ -436,8 +448,8 @@ sub score
 	my $linString=$_[1];
 	my $reportString=$_[2];
 	print " step #2 Computing variant's scores\n";
-	system("perl $haploCOV/LinToFeats.pl --infile $infile --outfile $infile\_features.csv")==0||die("no scores computed");
-	system("perl $haploCOV/report.pl --infile $infile\_features.csv --outfile $infile\_PASS.csv")==0||die("no report.pl applied"); 
+	system("perl $haploCOV/LinToFeats.pl --infile $infile --outfile $infile\_features.csv $linString")==0||die("LinToFeats.pl exited with an error. Please see above!");
+	system("perl $haploCOV/report.pl --infile $infile\_features.csv --outfile $infile\_PASS.csv $reportString")==0||die("report.pl exited with an error. Please see above!"); 
 	return("$infile\_PASS.csv","$infile\_features.csv");
 
 }
@@ -450,8 +462,8 @@ sub prevalence
 	my $increaseString=$_[3];
 
        print " step #3 prevalence based report \n";
-       system("perl $haploCOV/p_assign.pl --dfile $augment --metafile $input $assignString --out $input\_assigned.txt")==0||die("no assign\n");
-       system("perl $haploCOV/increase.pl --file $input\_assigned.txt $increaseString")==0||die("no report\n");
+       system("perl $haploCOV/p_assign.pl --dfile $augment --infile $input $assignString --outfile $input\_assigned.txt")==0||die("p_assign.pl exited with an error. Please see above!\n");
+       system("perl $haploCOV/increase.pl --file $input\_assigned.txt $increaseString")==0||die("increase.pl exited with an error. Please see above!\n");
        return("$input\_assigned.txt.prev","$input\_assigned.txt");
 
 }
@@ -463,17 +475,24 @@ sub process_locales
 	my $masterFile=$_[1];
 	my %out_conf=();
 
-	print "Preprocessing: generating area/locales files\n";
+	print "Preprocessing: subsetting data by areas/locales\n";
 
 	open(IN,$localeFile);
+	my $header=<IN>;
 	while(<IN>)
 	{
 		chomp();
 		my ($region,$qualifier,$dateStart,$dateEnd,$alleles)=(split(/\t/));
+		if ($region eq "" || $qualifier eq "" || $dateStart eq "" ||
+			$dateEnd eq "" || $alleles eq "")
+		{
+			die("\nCould not process the following line:\n$_\nPlease check your locales file carefully.\nEach line should have exactly 5 columns.\nEmpty lines or missing values are NOT allowed.\n\n");
+		}
+			
 		print "$region\t$qualifier\t$dateStart\t$dateEnd\n";
 		my $localeWrapping="";		
 		$localeWrapping="--$qualifier $region" if $region ne "world";
-		system("perl $haploCOV/subset.pl $localeWrapping --startD $dateStart --endD $dateEnd --infile $masterFile --outfile $region")==0||die("could not subset the data");
+		system("perl $haploCOV/subset.pl $localeWrapping --startD $dateStart --endD $dateEnd --infile $masterFile --outfile $region")==0||die("subset.pl exited with an error! Please see above!\n");
 	
 		my $isWC= check_exists_command('wc'); 
 		if ($isWC)
@@ -486,7 +505,7 @@ sub process_locales
 				die ( "Execution will halt! Too few data!\nPlease make sure that you spelled your region/country/area of interest and dates correctly.\nIf so, please broaden the scope of the analysis to include at least 50 entries\n");  
 			}
 		}else{	
-			 warn "$0 uses wc verify that input files are not empty.\nBut wc is not found in your system\nThis might cause errors!";
+			 warn "$0 uses wc verify that input files are not empty.\nBut wc is not found in your system\nThis might cause errors!\n";
 
 		}
 
@@ -515,10 +534,15 @@ sub process_parameters
 		if ($_=~/^--/)
 		{
 			my ($pname,$pvalue)=(split(/\t/));
+			unless ($pvalue)
+			{
+				die("Malformed string $_. I could not set the value for this parameter. Did you forget to specify the parameter value? are the fields separated by tab?\n");
+			}
 			$P=$pvalue if $pname eq "--minP";
 			$W=$pvalue if $pname eq "--nInt";
 			$parameters{$program}.=" $pname $pvalue";
 		}else{
+			next if $_=~/^#/;
 			$program=$_;
 			$parameters{$program}="";
 		}
@@ -547,6 +571,7 @@ sub check_arguments
                         warn("Valid arguments are @valid\n");
                         warn("All those moments will be lost in time, like tears in rain.\n Time to die!\n");
                         print_help();
+			die("Reason:\nInvalid parameter $act provided\n");
                 }
         }
 }
@@ -618,52 +643,38 @@ sub check_exists_command {
 
 sub print_help
 {
-        print " This utility is meant to:\n";
+        print " This utility is used to:\n";
         print " Automate the execution of the complete HaploCoV workflow\n";
-        print " Users need to provide;\n";
+        print " Users need to provide:\n";
         print " 1) a \"locales\" file, where they specify the timeframe and the geographic regions to include in the\n";
-	print " analyses. See the manual for more details\n";
-        print " 2) a metadata table, in HaploCoV format as produced by addToTable.pl\n";
-        print " 3) a configuration file, to specify the parameters used by each tool in HaploCoV\n";
-	print " see \"parameters\" in the main HaploCoV repository for an example\n";
-        print " 4) the path to their installation of HaploCoV\n";
-	print " 5) if an ancillary output file, with the list of novel variants identified by HaploCoV and their\n";
-       	print " defining genomic variants should be written as well\n";	
-        print " For every region/area/country specified in the \"locales\" a report will be produced (.rep file)\n";
-	print " the report will include the list of candidate new lineages that increased their \"vocness\" score\n";
-	print " or demonstrated an increase in prevalence in the area/region/country specified by the user\n";
-	print " if --varfile is set to a value greater than 0, an additional file with the complete list of the variants\n";
-	print " identified by HaploCoV and their defining genomic variants (.var) file will be produced as well\n"; 
-        print " --file and --locales are the mandatory parameter. Optional parameters include:\n";
-        print " --path and --param.  Both parameters have default values, which should work out of the box\n";
-	print " Please read the manual for more details\n";
+	print " analyses. See the manual for more details;\n";
+        print " 2) a metadata table, in HaploCoV format, as produced by addToTable.pl or NexStrainToHaploCoV.pl;\n";
+        print " 3) a parameters file, to specify the parameters used by each tool in HaploCoV\n";
+	print "    see \"parameters\" in the main HaploCoV repository for an example;\n";
+        print " 4) the path to their installation of HaploCoV;\n";
+	print " 5) (optional) specify  a \"Designations file\" with the list of novel variants identified\n"; 
+	print "    by HaploCoV and their defining genomic variants should be written. See --varfile.\n\n";	
+        print " For every region/area/country specified in the \"locales\" file a report will be produced (.rep file)\n";
+	print " the report will include the list of candidate new lineages that increased their \"VOC-ness\" score\n";
+	print " or demonstrated an increase in prevalence in the area/region/country specified by the user.\n";
+	print " If --varfile is set to \"a\" or \"b\", an additional file with the complete list of the variants\n";
+	print " identified by HaploCoV and their defining genomic variants (.var) file will be produced as well.\n"; 
+        print " \n\nThe mandatory parameters are: --file and --locales;\n";
+        print " --path, --varfile and --param are optional, and each has default values, which should work out of the box\n";
+	print " Please read the manual for more details\n\n";
         print "##INPUT PARAMETERS\n\n";
-        print "--file <<filename>>\t provides the metadata file\n";
-        print "--locales <<filename>>\t provides the \"locales\" file\n";
-        print "--param <<filnename>>\t configuration file, defaults to \"params\"\n";
-	print "--path <<path>>\t path to your HaploCoV installation, defaults to ./\n";
-	print "--varfile <<value>>\t n-> do not write this file; b->report only variants that passed both filters\n";
-	print "(score and prevalence); a-> report all the variants that passed any filter (score or prevalence or both\n";
-        print "\nTo run the program you MUST provide at least --file and -- locales\n";
+        print "--file <<filename>>\t HaploCoV formatted file;\n";
+        print "--locales <<filename>>\t \"locales\" file;\n";
+        print "--param <<filename>>\t configuration file, defaults to \"parameters\";\n";
+	print "--path <<path>>\t path to your HaploCoV installation, defaults to ./;\n";
+	print "--varfile <<value>>\t n-> do not write this file;\n";
+	print "                   \t b->report only variants that passed both filters (score and prevalence);\n"; 
+	print "                   \t a-> report all the variants that passed any filter (score or prevalence or both)\n";
+        print "\nTo run the program you MUST provide at least --file and --locales\n";
         print "both file needs to be in the current folder.\n\n";
         print "\n##EXAMPLE:\n\n";
-        print "1# input is metadata.tsv and locales.txt\nperl HaploCoV --file metadata.tsv --locales locales.txt\n\n";
+        print "1# input is metadata.tsv and locales.txt\nperl HaploCoV --file HaploCoV.tsv --locales locales.txt\n\n";
         print "2# output files, one per locale, will have the .rep extension\n";
+	print "3# designation files (--varfile, optional) will have a .var extension\n";
 }
-
-
-#my %configuration=%{process_locales("locMu","Colombia_mu")};
-#my %configuration=%{process_locales("locEpsilon","USA_epsilon")};
-#my %configuration=%{process_locales("locIota","USA_iota")};
-#my %configuration=%{process_locales("locEta","Nigeria_eta")};
-#my %configuration=%{process_locales("locDelta","India_delta")};
-#my %configuration=%{process_locales("locGamma","Braz_gamma")};
-#my %configuration=%{process_locales("locAlpha","EuUK_data_alpha")};
-#my %configuration=%{process_locales("locOmicron","SAF_omicron")};
-#my %configuration=%{process_locales("locBeta","SAF_beta")};
-
-#my %configuration=%{process_locales("completeLocales","referenceFile_assignedByMe.txt")};
-
-
-#my %configuration=%{process_locales("locAlpha","EuUK_data_alpha")};
 
